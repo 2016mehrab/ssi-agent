6e47cc288428e0c0ffa3d2b1d24e3cc85fbf3d4b
navid001 <navidalvi.001@gmail.com>
2024-05-25 13:59:05 +0600

    removed everything other than fabric

--- config/index.js
+++
@@ -1,8 +0,0 @@
-const { application } = require("express")
-const pkg = require("../package.json")
-module.exports = {
-    applicationName:pkg.name,
-    mongodb:{
-        url:process.env.MONGO_URI
-    }
-}
--- src/middlewares/auth-middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports.isAuthenticated = (req, res, next) => {
-  if (req.session.connection_id) {
-    next();
-  } else {
-    // res.status(401).send('You are not authorized to view this page');
-    req.session.returnTo = req.originalUrl;
-    res.redirect('/login');
-  }
-};
