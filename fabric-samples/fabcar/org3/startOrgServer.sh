42b1022f3bde262bee391c17b4870e21be62cbe0
navid001 <navidalvi.001@gmail.com>
2024-05-30 20:57:40 +0600

    fixed org3 issues

--- fabric-samples/fabcar/org3/startOrgServer.sh
+++ fabric-samples/fabcar/org3/startOrgServer.sh
@@ -1,9 +1,3 @@
-pushd ../../test-network/addOrg3/docker/
-docker-compose -f docker-compose-ca-org3.yaml up -d
-docker-compose -f docker-compose-couch-org3.yaml up -d
-docker-compose -f docker-compose-org3.yaml up -d
-popd
-
 echo Enrolling Admin
 node enrollAdmin.js
 echo Registering User
